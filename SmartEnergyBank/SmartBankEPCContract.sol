c6f8d5e08dabb0bfd98a1cd3b65f21bb6d7c89c0
venkatarenduchintala <venkatarenduchintala@users.noreply.github.com>
2018-02-15 12:11:31 +0100

    Intial Version
    Co-Authored-By: abhptta <abhptta@users.noreply.github.com>
    Co-Authored-By: ajpucher <ajpucher@users.noreply.github.com>

---
+++ SmartEnergyBank/SmartBankEPCContract.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+/**
+ *  Contract for the bank to interact with Energy producer companies to trade tokens for a beneficial financial information. 
+ */
+
+contract SmartBankEPCContract {
+    // public variables
+
+    // constructor
+
+    // methods /operations
+
+
+}
