62d9dcd6505756210a85870ea04aa99fc17bdb4f
venkatarenduchintala <venkatarenduchintala@users.noreply.github.com>
2018-02-15 15:01:03 +0100

    Added storedData

--- SmartEnergyBank/SmartBankEPCContract.sol
+++ SmartEnergyBank/SmartBankEPCContract.sol
@@ -1,13 +1,15 @@
-pragma solidity ^0.4.17;
+
 /**
  *  Contract for the bank to interact with Energy producer companies to trade tokens for a beneficial financial information. 
  */
-
+pragma solidity ^0.4.17;    
+/// @title Contract between EPC and Bank 
 contract SmartBankEPCContract {
     // public variables
+    uint storedData public ;
 
     // constructor
-
+    
     // methods /operations
 
 
